00614e48f5ce8176e6d763438a67bb02e22a72b5
Kera505 <kereme8194@gmail.com>
2024-01-31 03:01:13 +0400

    Compile time process done.

---
+++ Week 1/Lesson 1/test.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace Test{
+ 
+     class Program{
+
+       public static void Main(){
+       Console.WriteLine("Hello World");
+       Console.ReadLine();
+
+     }
+ 
+  } 
+
+}
