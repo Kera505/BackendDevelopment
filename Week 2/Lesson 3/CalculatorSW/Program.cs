f0a7ca7cb4b0058d03e6e2216c6f1980432c78f1
Kera505 <kereme8194@gmail.com>
2024-02-04 14:25:13 +0400

    New Calculator

---
+++ Week 2/Lesson 3/CalculatorSW/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorSW
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("1 - ci ededi daxil edin.");
+            int x1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("2 - ci ededi daxil edin.");
+            int x2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Simvolu daxil edin.( +  -  *  / )");
+            char sign = Convert.ToChar(Console.ReadLine());
+
+            switch (sign)
+            {
+
+                case '+':
+                    Console.WriteLine($"{x1} {sign} {x2} = {x1 + x2}");
+                    Console.ReadLine();
+                    break;
+                case '-':
+                    Console.WriteLine($"{x1} {sign} {x2} = {x1 - x2}");
+                    Console.ReadLine();
+                    break;
+                case '*':
+                    Console.WriteLine($"{x1} {sign} {x2} = {x1 * x2}");
+                    Console.ReadLine();
+                    break;
+                case '/':
+                    Console.WriteLine($"{x1} {sign} {x2} = {x1 / x2}");
+                    Console.ReadLine();
+                    break;
+                default:
+                    Console.WriteLine(" Bele bir simvol sistemde mövcud deyildir ! ");
+                    Console.ReadLine();
+                    break;
+            }
+        }
+    }
+}
---
+++ Week 2/Lesson 3/CalculatorSW/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Bir bütünleştirilmiş koda ilişkin Genel Bilgiler aşağıdaki öznitelikler kümesiyle
+// denetlenir. Bütünleştirilmiş kod ile ilişkili bilgileri değiştirmek için
+// bu öznitelik değerlerini değiştirin.
+[assembly: AssemblyTitle("CalculatorSW")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP")]
+[assembly: AssemblyProduct("CalculatorSW")]
+[assembly: AssemblyCopyright("Copyright © HP 2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible özniteliğinin false olarak ayarlanması bu bütünleştirilmiş koddaki türleri
+// COM bileşenleri için görünmez yapar. Bu bütünleştirilmiş koddaki bir türe
+// erişmeniz gerekirse ComVisible özniteliğini o türde true olarak ayarlayın.
+[assembly: ComVisible(false)]
+
+// Bu proje COM'un kullanımına sunulursa, aşağıdaki GUID tür kitaplığının kimliği içindir
+[assembly: Guid("09327ecc-f1c6-4289-a40f-a3404efe1aa7")]
+
+// Bir derlemenin sürüm bilgileri aşağıdaki dört değerden oluşur:
+//
+//      Ana Sürüm
+//      İkincil Sürüm 
+//      Yapı Numarası
+//      Düzeltme
+//
+// Tüm değerleri belirtebilir veya varsayılan Derleme ve Düzeltme Numaralarını kullanmak için
+// aşağıda gösterildiği gibi '*' kullanabilirsiniz:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
