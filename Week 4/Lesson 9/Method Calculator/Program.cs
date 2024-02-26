328a8f54b384c61ef320dd3d571c081192870295
Kera505 <kereme8194@gmail.com>
2024-02-26 06:27:04 +0400

    New lessons added and completed.

---
+++ Week 4/Lesson 9/Method Calculator/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine(" 1-CI EDEDI DAXIL EDIN : ");
+                int a = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine(" 2-CI EDEDI DAXIL EDIN : ");
+                int b = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine(" SIMVOLU DAXIL EDIN : ");
+                char option = Convert.ToChar(Console.ReadLine());
+                Calculator(a, b, option);
+            }
+        }
+
+        public static void Calculator(int a, int b, char option)
+        {
+            switch (option)
+            {
+                case '-':
+                    Console.WriteLine($" CAVAB : {a - b}");
+                    break;
+                case '+':
+                    Console.WriteLine($" CAVAB : {a + b}");
+                    break;
+                case '*':
+                    Console.WriteLine($" CAVAB : {a * b}");
+                    break;
+                case '/':
+                    Console.WriteLine($" CAVAB : {a / b}");
+                    break;
+                default:
+                    Console.WriteLine("Bele bir isare movcud deyil ! ");
+                    break;
+                }
+        }
+    }
+    
+}
---
+++ Week 4/Lesson 9/Student Class/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Student_Class
+{
+    internal class Program
+    {
+        public class Student
+        {
+            public string Name { get; set; }
+            public string Surname { get; set; }
+            public byte Age { get; set; }
+        }
+
+        static void Main(string[] args)
+        {
+            List<Student> students = new List<Student>();
+            while (true)
+            {
+                Console.WriteLine("Enter the student name: ");
+                string nm = Console.ReadLine();
+                Console.WriteLine("Enter the student surname: ");
+                string srnm = Console.ReadLine();
+                Console.WriteLine("Enter the student age: ");
+                byte ag = Convert.ToByte(Console.ReadLine());
+
+                Student student = new Student();
+                student.Name = nm;
+                student.Surname = srnm;
+                student.Age = ag;
+               students.Add(student);
+            }
+        }
+    }
+}
+
